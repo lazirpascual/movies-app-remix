1fe1ca5ca02c3e8decf94928772b4669f5daeec1
Lazir Pascual <lazirpascual@gmail.com>
2023-12-22 02:34:57 -0500

    add movies section in index page

--- app/root.tsx
+++ app/root.tsx
@@ -15,11 +15,13 @@ export const links: LinksFunction = () => [
   { rel: "stylesheet", href: stylesheet },
 ];
 
-export const meta: MetaFunction = () => [{
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
-}];
+export const meta: MetaFunction = () => [
+  {
+    charset: "utf-8",
+    title: "New Remix App",
+    viewport: "width=device-width,initial-scale=1",
+  },
+];
 
 export default function App() {
   return (
@@ -40,15 +42,15 @@ export default function App() {
   );
 }
 
-function Layout({children}: {children: ReactNode}) {
+function Layout({ children }: { children: ReactNode }) {
   return (
     <>
-    <nav className="px-10 pt-5">
-      <Link to="/" prefetch="intent" className="text-2xl font-semibold">
-        Move<span className="text-teal-500">DB</span>
-      </Link>
-    </nav>
-    <main>{children}</main>
+      <nav className="px-10 pt-5">
+        <Link to="/" prefetch="intent" className="text-2xl font-semibold">
+          Move<span className="text-teal-500">DB</span>
+        </Link>
+      </nav>
+      <main>{children}</main>
     </>
-  )
+  );
 }
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,7 @@
 import { useLoaderData } from "@remix-run/react";
 import { LoaderFunctionArgs } from "@remix-run/server-runtime";
 import { json } from "react-router";
-
+import { Link } from "@remix-run/react";
 
 export function headers({
   loaderHeaders,
@@ -21,36 +21,64 @@ export function headers({
 }
 
 export async function loader({}: LoaderFunctionArgs) {
-  const url = await  fetch(
-    'https://api.themoviedb.org/3/trending/movie/day?language=en-US',
+  const url = await fetch(
+    "https://api.themoviedb.org/3/trending/movie/day?language=en-US",
     {
       headers: {
         accept: "application/json",
         Authorization:
-         "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMzBhMmQ0ZjcyYmQwNjc1NzhiNjY4NmJjOWY5YWQ2MCIsInN1YiI6IjY1ODUyZjM1MDFiMWNhNWZhNzkwMTE3MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.V0D3-TwfI7w3E-PH8ouQ2EqIDJ731WcTb4SCiYMdbMg"
-      }
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMzBhMmQ0ZjcyYmQwNjc1NzhiNjY4NmJjOWY5YWQ2MCIsInN1YiI6IjY1ODUyZjM1MDFiMWNhNWZhNzkwMTE3MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.V0D3-TwfI7w3E-PH8ouQ2EqIDJ731WcTb4SCiYMdbMg",
+      },
     }
-  )
+  );
 
-  return json(await url.json())
+  return json(await url.json());
 }
 
 export default function Index() {
   const data = useLoaderData();
-  console.log(data);
+  const movieDataMarkdown = data.results.map((movie: any) => (
+    <div className="flex flex-col overflow-hidden rounded-lg border bg-white">
+      <Link
+        to={`movie/${movie.id}/comments`}
+        prefetch="intent"
+        className="group relative block h-48 overflow-hidden bg-gray-100 md:h-64"
+      >
+        <img
+          src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+          alt=""
+          className="absolute inset-0 h-full w-full object-cover object-center transition duration-2BB group-hover:scale-110"
+        />
+      </Link>
+
+      <div className="flex flex-1 flex-col p-4 sm:p-6">
+        <h2 className="mb-2 text-lg font-semibold text-gray-800">
+          <Link
+            to={`movie/${movie.id}/comments`}
+            prefetch="intent"
+            className="transition duration-100 hover:text-indigo-500 active:text-indigo-600"
+          >
+            {movie.title}
+          </Link>
+          <p className="text-gray-500 line-clamp-3">{movie.overview}</p>
+        </h2>
+      </div>
+    </div>
+  ));
+
   return (
-   <div className="bg-white py-6 sm:py-8 lg:py-12">
-     <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
-       <div className="mb-10 md:mb-16">
+    <div className="bg-white py-6 sm:py-8 lg:py-12">
+      <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
+        <div className="mb-10 md:mb-16">
           <h2 className="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg">
             Top Trending Movies
           </h2>
-       </div>
+        </div>
       </div>
 
       <div className="grid gap-4 sm:grid-cols-2 md:gap-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-8">
-
+        {movieDataMarkdown}
       </div>
-   </div>
+    </div>
   );
 }
